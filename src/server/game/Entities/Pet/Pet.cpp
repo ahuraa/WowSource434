7effe2ba6cb10ed9309528fca8464b355adc9e3f
vanasis <v.b_hellscream@yahoo.com>
2015-02-13 00:50:24 +0330

    Remove Unused old DrW

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -971,12 +971,6 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                     SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, maxDamage);
                     break;
                 }
-				case 27893: // Dancing Rune Weapon
-				{
-					SetRedirectThreat(100, m_owner->GetGUID());
-					CastSpell(this, 51906, true);
-					break;
-				}
                 case 89:                // Infernal
                 {
                     if (m_owner->GetTypeId() == TYPEID_PLAYER)
@@ -2130,11 +2124,6 @@ void Guardian::RecalculatePetScalingDamageDone()
         auraId = 54566;
         effIndex = EFFECT_2;
     }
-	else if (GetEntry() == 27893)
-	{
-		auraId = 51906;
-		effIndex = EFFECT_0;
-	}
     else if (m_owner && m_owner->getClass() == CLASS_WARLOCK)
     {
         auraId = 34947;
@@ -2174,8 +2163,6 @@ void Guardian::RecalculatePetScalingAttackSpeed(WeaponAttackType att)
         auraId = 89446;
     else if (IsPetGhoul())
         auraId = 51996;
-	else if (GetEntry() == 27893)
-		auraId = 51906;
     else if (GetEntry() == 510) // water elemental
         auraId = 89764;
 
