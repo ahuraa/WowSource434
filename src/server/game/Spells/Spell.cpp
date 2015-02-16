c1682fd055f337bff1d454f7a3598860e6ae053d
mastergku <sergioausin1993@gmail.com>
2015-02-16 20:49:25 +0100

    Core/Player
    Fixed the exploit that allowed player to buy several items, and
    duplicate them

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3388,7 +3388,12 @@ void Spell::cancel()
 void Spell::cast(bool skipCheck)
 {
     // update pointers base at GUIDs to prevent access to non-existed already object
-    UpdatePointers();
+	if (!UpdatePointers())
+		 {
+		       // cancel the spell if UpdatePointers() returned false, something wrong happened there
+			cancel();
+		return;
+		}
 
     // cancel at lost explicit target during cast
     if (m_targets.GetObjectTargetGUID() && !m_targets.GetObjectTarget())
@@ -3746,7 +3751,12 @@ void Spell::handle_immediate()
 
 uint64 Spell::handle_delayed(uint64 t_offset)
 {
-    UpdatePointers();
+	if (!UpdatePointers())
+	 {
+	     // finish the spell if UpdatePointers() returned false, something wrong happened there
+			finish(false);
+		return 0;
+	}
 
     if (m_caster->GetTypeId() == TYPEID_PLAYER)
         m_caster->ToPlayer()->SetSpellModTakingSpell(this, true);
@@ -3900,7 +3910,12 @@ void Spell::SendSpellCooldown()
 void Spell::update(uint32 difftime)
 {
     // update pointers based at it's GUIDs
-    UpdatePointers();
+	if (!UpdatePointers())
+	{
+	       // cancel the spell if UpdatePointers() returned false, something wrong happened there
+		cancel();
+		return;
+	}
 
     if (m_targets.GetUnitTargetGUID() && !m_targets.GetUnitTarget())
     {
@@ -5010,6 +5025,7 @@ void Spell::TakeReagents()
             }
 
             m_CastItem = NULL;
+			m_castItemGUID = 0;
         }
 
         // if GetItemTarget is also spell reagent
@@ -6978,7 +6994,7 @@ void Spell::DelayedChannel()
     SendChannelUpdate(m_timer);
 }
 
-void Spell::UpdatePointers()
+bool Spell::UpdatePointers()
 {
     if (m_originalCasterGUID == m_caster->GetGUID())
         m_originalCaster = m_caster;
@@ -6989,14 +7005,18 @@ void Spell::UpdatePointers()
             m_originalCaster = NULL;
     }
 
-    if (m_castItemGUID && m_caster->GetTypeId() == TYPEID_PLAYER)
-        m_CastItem = m_caster->ToPlayer()->GetItemByGuid(m_castItemGUID);
+	if (m_castItemGUID && m_caster->GetTypeId() == TYPEID_PLAYER){
+		m_CastItem = m_caster->ToPlayer()->GetItemByGuid(m_castItemGUID);
+		// cast item not found, somehow the item is no longer where we expected
+		if (!m_CastItem)
+			return false;
+	}
 
     m_targets.Update(m_caster);
 
     // further actions done only for dest targets
     if (!m_targets.HasDst())
-        return;
+        return true;
 
     // cache last transport
     WorldObject* transport = NULL;
@@ -7017,6 +7037,7 @@ void Spell::UpdatePointers()
             dest._position.RelocateOffset(dest._transportOffset);
         }
     }
+	return true;
 }
 
 CurrentSpellTypes Spell::GetCurrentContainer() const
