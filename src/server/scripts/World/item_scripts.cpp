2cb5ef73c0b50e0bfee3828d5e4c8a45da4f57e4
mastergku <sergioausin1993@gmail.com>
2015-02-22 20:56:01 +0100

    Core/Item: fix for item 62237
    Fixed the item that was asked here:
    http://wowsource.info/index.php?/topic/2851-adventurers-journal-inscription-item/

--- src/server/scripts/World/item_scripts.cpp
+++ src/server/scripts/World/item_scripts.cpp
@@ -104,6 +104,59 @@ public:
     }
 };
 
+
+class item_adventurers_journal : public ItemScript
+{
+public:
+	item_adventurers_journal() : ItemScript("item_adventurers_journal") { }
+
+	bool OnUse(Player* player, Item* item, SpellCastTargets const& targets)
+	{
+		if (player->getLevel() < 85){
+			int number = rand() % 10;
+			uint32 count = 1;
+
+			player->DestroyItemCount(item, count, true);
+			switch (number)
+			{
+			case 0:
+				player->CastSpell(player, 86972, true);
+				break;
+			case 1:
+				player->CastSpell(player, 86963, true);
+				break;
+			case 2:
+				player->CastSpell(player, 86988, true);
+				break;
+			case 3:
+				player->CastSpell(player, 86977, true);
+				break;
+			case 4:
+				player->CastSpell(player, 86992, true);
+				break;
+			case 5:
+				player->CastSpell(player, 86980, true);
+				break;
+			case 6:
+				player->CastSpell(player, 86975, true);
+				break;
+			case 7:
+				player->CastSpell(player, 86983, true);
+				break;
+			case 8:
+				player->CastSpell(player, 86976, true);
+				break;
+			case 9:
+				player->CastSpell(player, 86982, true);
+				break;
+			default:
+				break;
+			}
+		}
+		return false;
+	}
+};
+
 /*#####
 # item_gor_dreks_ointment
 #####*/
@@ -423,4 +476,5 @@ void AddSC_item_scripts()
     new item_dehta_trap_smasher();
     new item_trident_of_nazjan();
     new item_captured_frog();
+	new item_adventurers_journal();
 }
